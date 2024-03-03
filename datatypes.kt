6634c224e4d406d6d67a7a6a08b7425b76eddab5
XSHOAIB <145601395+XSHOAIB@users.noreply.github.com>
2024-03-04 02:46:13 +0530

    Update datatypes.kt

--- datatypes.kt
+++ datatypes.kt
@@ -4,4 +4,12 @@ fun main(){
   val myLetter = 'D'        // Char
   val myBoolean = true      // Boolean
   val myText = "Hello"      // String
+  val myNum: Int = 5                // Int
+  val myDoubleNum: Double = 5.99    // Double
+  val myLetter: Char = 'D'          // Char
+  val myBoolean: Boolean = true     // Boolean
+  val myText: String = "Hello"      // String
+  val myNu: Byte = 100
+  val myn: Short = 5000
+  
 }
